221448dfc17f53296c0315890dc6124e5968b2bb
Jorge-Zea <jzea8810@gmail.com>
2024-10-04 23:19:14 -0500

    Busqueda y filtro por categorias
    Activo la función de búsqueda y el filtrado de productos  por categorías.

--- react-app/src/componentes/lista_de_productos/ListaDeProductos.js
+++ react-app/src/componentes/lista_de_productos/ListaDeProductos.js
@@ -3,22 +3,26 @@ import ProductoDetalle from "../productoDetalle/ProductoDetalle";
 function ListaDeProductos(props) {
     return (  
         <section id="carrusel-productos">
-      <h2>Resultados de productos</h2>
-        <div class="carrusel">
-            <div id="lista-productos" class="productos-carrusel">
-                <ProductoDetalle nombre={"Medicamentos"} precio={1900} etiquetaPrecio={"$1,900"} imagen={"img/Mylanta.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"cuidado personal"} precio={5900} etiquetaPrecio={"$5,900"} imagen={"img/balsamo-labial.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Vitaminas y suplementos"} precio={99200} etiquetaPrecio={"$99,200"} imagen={"img/Ensure.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Equipo médico"}  precio={15000} etiquetaPrecio={"$15,000"} imagen={"img/termometro.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Medicamento"} precio={1900} etiquetaPrecio={"$1,900"} imagen={"img/Azitromicina-MK-suspension-200mg-x15ml.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Paracetamol"} precio={10000}  etiquetaPrecio={"$10,000"} imagen={"img/Paracetamol.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Acetaminofen"} precio={2500} etiquetaPrecio={"$2,500"} imagen={"img/Acetaminofen.jpg"} disponible={false} agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Vitaminas y suplementos"} precio={20000} etiquetaPrecio={"$20,000"} imagen={"img/vitaminaC.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Cuidado personal"} precio={5000} etiquetaPrecio={"$5,000"} imagen={"img/Cepillo oral B.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-                <ProductoDetalle nombre={"Equipo médico"} precio={40700} etiquetaPrecio={"$40,700"} imagen={"img/tensiometro_digital.jpg"} disponible agregarProducto={props.agregarProducto}></ProductoDetalle>
-            </div>
-        </div>
-    </section>
+        <h2>Resultados de productos</h2>
+          <div class="carrusel">
+              <div id="lista-productos" class="productos-carrusel">
+              {props.productosFiltrados.length > 0 ? (
+          props.productosFiltrados.map((producto) => (
+  
+              <ProductoDetalle nombre={producto.nombre} 
+              etiquetaPrecio={producto.etiquetaPrecio}
+              imagen={producto.imagen}
+              busqueda={props.busqueda} 
+              disponible={producto.disponible}
+              agregarProducto={props.agregarProducto}
+              ></ProductoDetalle>
+          ))
+        ) : (
+          <p>No se encontraron productos</p>
+        )}
+              </div>
+          </div>
+      </section>
     
     );
 }
